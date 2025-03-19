0404a085e0ce9d688cb5e148e8f74160faf61eaa
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2025-03-19 09:39:49 +0100

    update comment

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,4 +1,4 @@
-// https://fontsource.org/fonts/overpass/install
+// https://fontsource.org/fonts/red-hat-display/install
 //
 // Remember to update `--font-sans` in theme.css
 import '@fontsource-variable/red-hat-display'
